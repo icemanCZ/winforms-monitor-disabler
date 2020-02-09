943c0a1232813e374acea45dbcabef91dc306d51
Tomas Horak <tom1horak@seznam.cz>
2020-02-09 12:04:19 +0100

    Add project files.

---
+++ winforms-monitor-disabler/Form1.cs
@@ -0,0 +1,41 @@
+﻿using GlobalHotkeys;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ice.MonitorDisabler
+{
+    public partial class Form1 : Form
+    {
+        private GlobalHotkey ghk;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ghk = new GlobalHotkey(Modifiers.NoMod, Keys.Escape, this);
+            ghk.Register();
+            Native.SetMonitorEnable(this.Handle, false);
+            this.Shown += (o, e) => this.Hide();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            var hotkeyInfo = HotkeyInfo.GetFromMessage(m);
+            if (hotkeyInfo != null) HotkeyProc(hotkeyInfo);
+            base.WndProc(ref m);
+        }
+
+        private void HotkeyProc(HotkeyInfo hotkeyInfo)
+        {
+            Native.SetMonitorEnable(this.Handle, true);
+            Application.Exit();
+        }
+    }
+}
---
+++ winforms-monitor-disabler/Native.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ice.MonitorDisabler
+{
+    public class Native
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct PHYSICAL_MONITOR
+        {
+            public IntPtr hPhysicalMonitor;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szPhysicalMonitorDescription;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "MonitorFromWindow", SetLastError = true)]
+        private static extern IntPtr MonitorFromWindow([In] IntPtr hwnd, uint dwFlags);
+
+        [DllImport("dxva2.dll", EntryPoint = "SetVCPFeature", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetVCPFeature(IntPtr hMonitor, byte bVCPCode, short dwNewValue);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetPhysicalMonitorsFromHMONITOR", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetPhysicalMonitorsFromHMONITOR(IntPtr hMonitor, uint dwPhysicalMonitorArraySize, [Out] PHYSICAL_MONITOR[] pPhysicalMonitorArray);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetNumberOfPhysicalMonitorsFromHMONITOR", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetNumberOfPhysicalMonitorsFromHMONITOR(IntPtr hMonitor, ref uint pdwNumberOfPhysicalMonitors);
+
+        private const byte PowerMode = 0xD6;  // VCP Code defined in VESA Monitor Control Command Set (MCCS) standard
+        private const short PowerOn = 0x01;
+        private const short PowerOff = 0x04;
+
+        public static void SetMonitorEnable(IntPtr windowHandle, bool enable)
+        {
+            uint pdwNumberOfPhysicalMonitors = 0;
+            IntPtr hMonitor = MonitorFromWindow(windowHandle, 1);
+            bool numberOfPhysicalMonitorsFromHmonitor = GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor, ref pdwNumberOfPhysicalMonitors);
+            var pPhysicalMonitorArray = new PHYSICAL_MONITOR[pdwNumberOfPhysicalMonitors];
+            GetPhysicalMonitorsFromHMONITOR(hMonitor, pdwNumberOfPhysicalMonitors, pPhysicalMonitorArray);
+            SetVCPFeature(pPhysicalMonitorArray[0].hPhysicalMonitor, PowerMode, enable ? PowerOn : PowerOff);
+        }
+    }
+}
